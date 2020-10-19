2ae1993e0b5178c0af6f38b039fe3498aa07bb58
emma <emma@emma-Lenovo-ideapad-130-15AST>
2020-10-19 16:15:58 +0200

    gymmanagementsystem front end pro

---
+++ gymmanagementsystemvue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <EmployeeRegistration />
+  </div>
+</template>
+
+<script>
+
+import EmployeeRegistration from './components/EmployeeRegistration.vue';
+export default {
+  name: 'App',
+  components: {
+   EmployeeRegistration
+  }
+}
+</script>
+
+<style>
+
+
+</style>
